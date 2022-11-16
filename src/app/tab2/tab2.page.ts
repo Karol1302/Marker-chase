355111f6fc1a4170c43e00671ca1d04f1b556297
KarolBrz <karol130013b@gmail.com>
2022-11-16 17:34:47 +0100

    leaflet

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -12,13 +12,13 @@ export class Tab2Page {
 
   ionViewDidEnter(){
     this.map = L.map('mapId').setView([49.7879,19.1869], 11);
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution:
      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(this.map);
-    const markPoint = L.marker([49.7879,19.1869]);
-    markPoint.bindPopup('<p>Leaflet</p>')
-    this.map.addlayer(markPoint);
+    L.control.locate().addTo(this.map);
   }
+  
 
 }
