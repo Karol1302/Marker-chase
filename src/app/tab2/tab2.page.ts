0a2280e685eb8639869dccc01d028f910c77df5d
KarolBrz <karol130013b@gmail.com>
2022-11-24 20:30:42 +0100

    Loc-refresh

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 import * as L from 'leaflet';
+import { icon, Marker } from 'leaflet';
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+Marker.prototype.options.icon = iconDefault;
+
 
 @Component({
   selector: 'app-tab2',
@@ -11,19 +27,57 @@ export class Tab2Page {
   constructor() {}
 
   ionViewDidEnter(){
-    this.map = L.map('mapId').setView([49.7879,19.1869], 11);
+   var map = L.map('mapId').setView([49.7879,19.1869], 11);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution:
      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(this.map);
-    L.control.locate().addTo(this.map);
+  }).addTo(map);
+    // L.control.locate().addTo(map);
+    // var markerOne = L.marker([49.7879,19.1869]);
+    // markerOne.addTo(this.map);
 
-    var markerOne = L.marker([49.7879,19.1869]);
-    markerOne.addTo(this.map);
+    // var markerTwo = L.marker([49.7727974,19.0603719]);
+    // markerTwo.addTo(this.map);
 
-    var markerTwo = L.marker([49.7727974,19.0603719]);
-    markerTwo.addTo(this.map);
+    var toolbar = L.Toolbar();
+    toolbar.addToolbar(map);
+
+    if(!navigator.geolocation)
+    console.log("Nie udało się pobrać lokalizacji")
+    else
+    {
+      setInterval(() => {
+        navigator.geolocation.getCurrentPosition(getPosition)
+      }, 5000);
+    }
+
+    var userMarker, userAccuracyCircle
+
+    function getPosition(position){
+      // console.log(position)
+      
+      var lat = position.coords.latitude
+      var long = position.coords.longitude
+      var accuracy = position.coords.accuracy
+
+      if(userMarker)
+      {
+        map.removeLayer(userAccuracyCircle)
+      }
+      if(userAccuracyCircle)
+      {
+        map.removeLayer(userMarker)
+      }
+
+      userMarker = L.marker([lat, long]),
+      userAccuracyCircle = L.circle([lat, long], {radius: accuracy})
+
+      var featureGroup = L.featureGroup([userMarker, userAccuracyCircle]).addTo(map)
+      map.fitBounds(featureGroup.getBounds())
+
+      console.log( "Latitude: " +lat + " Longitude: " +long +" Accuracy: " +accuracy)
+    }
 
   }
   
