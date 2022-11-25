694cdc49f06376da430904f0de64b17ae336a9b4
KarolBrz <karol130013b@gmail.com>
2022-11-25 01:38:13 +0100

    Fix: fit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import * as L from 'leaflet';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
+import 'leaflet-easybutton';
 import { icon, Marker } from 'leaflet';
 const iconRetinaUrl = 'assets/marker-icon-2x.png';
 const iconUrl = 'assets/marker-icon.png';
@@ -39,24 +42,39 @@ export class Tab2Page {
 
     // var markerTwo = L.marker([49.7727974,19.0603719]);
     // markerTwo.addTo(this.map);
+    
+    // var toolbar = L.Toolbar();
+    // toolbar.addToolbar(map);
 
-    var toolbar = L.Toolbar();
-    toolbar.addToolbar(map);
+    navigator.geolocation.getCurrentPosition(getPosition)
 
     if(!navigator.geolocation)
     console.log("Nie udało się pobrać lokalizacji")
     else
     {
       setInterval(() => {
-        navigator.geolocation.getCurrentPosition(getPosition)
+        navigator.geolocation.getCurrentPosition(coords)
       }, 5000);
     }
 
     var userMarker, userAccuracyCircle
+  
+    function getPosition(position)
+    {
+      var lat = position.coords.latitude
+      var long = position.coords.longitude
+      var accuracy = position.coords.accuracy
 
-    function getPosition(position){
-      // console.log(position)
-      
+      userMarker = L.marker([lat, long]),
+      userAccuracyCircle = L.circle([lat, long], {radius: accuracy})
+
+      var featureGroup = L.featureGroup([userMarker, userAccuracyCircle]).addTo(map)
+      map.fitBounds(featureGroup.getBounds())
+      console.log( "Latitude: " +lat + " Longitude: " +long +" Accuracy: " +accuracy)
+    }
+
+    function coords(position)
+    {
       var lat = position.coords.latitude
       var long = position.coords.longitude
       var accuracy = position.coords.accuracy
@@ -72,12 +90,15 @@ export class Tab2Page {
 
       userMarker = L.marker([lat, long]),
       userAccuracyCircle = L.circle([lat, long], {radius: accuracy})
-
       var featureGroup = L.featureGroup([userMarker, userAccuracyCircle]).addTo(map)
-      map.fitBounds(featureGroup.getBounds())
 
-      console.log( "Latitude: " +lat + " Longitude: " +long +" Accuracy: " +accuracy)
     }
+    L.easyButton('<img src="/path/to/img/of/penguin.png">', function(btn, map){
+      var antarctica = [-77,70];
+      map.setView(this.antarctica);
+  }).addTo( map );
+  
+
 
   }
   
